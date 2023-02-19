890b1af5d7d55642df44efccc6a923d48d36d459
Pierre Loviton <74408560+lovitonp10@users.noreply.github.com>
2023-02-19 17:19:35 +0100

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,6 +1,16 @@
-import argparse
+import os
+import requests
+import io
+import pandas as pd
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--output", type=str, required=True)
-    args = parser.parse_args()
+if not os.path.exists('data'):
+    os.makedirs('data/')
+
+# Binary Alphadigits 
+
+#if not os.path.exists('data/dpe-tertiaire.csv'):
+url = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-tertiaire/full"
+
+# Envoi de la requête
+response = requests.get(url)
+pd.read_csv(io.StringIO(response.text)).to_csv('data/data.csv')
